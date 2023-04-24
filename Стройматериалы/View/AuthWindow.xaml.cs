88b45c39e464e64608fdd26c540fa7cc8a3ded10
Your Name <you@example.com>
2023-04-24 23:22:56 +0300

    first commit

---
+++ Стройматериалы/View/AuthWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Стройматериалы.ViewModel;
+
+namespace Стройматериалы.View
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        private AuthViewModel _authViewModel;
+        public AuthWindow()
+        {
+            InitializeComponent();
+            DataContext = _authViewModel= new AuthViewModel();
+        }
+
+        private void TbLog_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            (DataContext as AuthViewModel).Login = TbLog.Text;
+        }
+
+        private void PsBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            (DataContext as AuthViewModel).Password = PsBox.Password;
+        }
+
+        private void BtnAuth_Click(object sender, RoutedEventArgs e)
+        {
+            _authViewModel.Auth();
+        }
+    }
+}
---
+++ Стройматериалы/ViewModel/AuthViewModel.cs
@@ -0,0 +1,75 @@
+﻿using KininTech.SqlMapping.ORM;
+using SqlKata;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations.Builders;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Стройматериалы.Entity;
+using Стройматериалы.Model;
+
+namespace Стройматериалы.ViewModel
+{
+    public class AuthViewModel : BaseViewModel
+    {
+        private string _login;
+        private string _password;
+        public BuildingMaterialEntities _db;
+        private AuthViewModel _authViewModel;
+
+        public AuthViewModel()
+        {
+            _db = new BuildingMaterialEntities();
+            _authViewModel = new AuthViewModel();
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public string Login
+        {
+            get => _login;
+            set
+            {
+                _login = value;
+                OnPropertyChanged(nameof(Login));
+            }
+        }
+
+       public async Task<bool> Helper(string log,string password)
+        {
+            var user = await _db.User.FirstOrDefaultAsync(x => x.Password == password && x.Login == log);
+            if (user == null)
+            {
+                return false;
+            }
+            UserSingletone.User=user;
+            return true;
+        }
+
+        public async void Auth()
+        {
+            if (await _authViewModel.Helper(Login, Password))
+            {
+                MessageBox.Show("Авторизация прошла успешно");
+                
+            }
+            else
+            {
+                MessageBox.Show("Что-то пошло не так , повторите попытку позже");
+            }
+        }
+
+    }
+}
